2eda7e6098e3c22644734fe3635c2267994b4403
Jacek Sysiak <j4sysiak@gmail.com>
2022-12-17 21:15:47 +0100

    17-12-2022 20:52

--- spring-boot-rest/src/main/java/com/baeldung/SpringBootRestApplication.java
+++ spring-boot-rest/src/main/java/com/baeldung/SpringBootRestApplication.java
@@ -1,13 +1,17 @@
 package com.baeldung;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class SpringBootRestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootRestApplication.class, args);
     }
-
 }
